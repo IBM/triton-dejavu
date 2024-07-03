1952d8787dd3e46ed455b253f1ec34d83e426755
Burkhard Ringlein <ngl@zurich.ibm.com>
2024-07-03 15:25:53 +0200

    code attributions

--- tests/rms_norm.py
+++ tests/rms_norm.py
@@ -1,5 +1,6 @@
 #  /*******************************************************************************
 #   * Copyright 2024 IBM Corporation
+#   * Copyright 2023 Daniel Han-Chen & the Unsloth team
 #   *
 #   * Licensed under the Apache License, Version 2.0 (the "License");
 #   * you may not use this file except in compliance with the License.
