d0004fb7acd427a460498ec9fe2593afb5f7ce5b
Burkhard Ringlein <ngl@zurich.ibm.com>
2024-07-16 05:43:40 -0400

    implemented passthrough mode

--- triton_dejavu/autotuner.py
+++ triton_dejavu/autotuner.py
@@ -40,6 +40,8 @@ from triton_dejavu.dejavu_storage import (
 )
 
 
+__additional_config_arg_check__ = ['num_warps', 'num_stages']
+
 # to be compatible with different triton 3.x versions
 def _all_kwargs(self):
     if not hasattr(self, "maxnreg"):
@@ -90,6 +92,7 @@ class Autotuner(KernelInterface):
         # the key hash is not covered by fn.hash!
         self.key_hash = get_list_hash(key)
         self.configs_len = len(self.configs)
+        self.config_kw_names = list(self.configs[0].kwargs.keys())
         self.key_idx = [arg_names.index(k) for k in key]
         self.arg_names = arg_names
 
@@ -280,88 +283,98 @@ class Autotuner(KernelInterface):
                 return [float("inf"), float("inf"), float("inf")]
 
     def run(self, *args, **kwargs):
-        self.nargs = dict(zip(self.arg_names, args))
-        used_cached_result = True
-        self.bench_time = 0.0
-        if len(self.configs) > 1:
-            all_args = {**self.nargs, **kwargs}
-            _args = []
-            for name in self.arg_names:
-                if name in all_args:
-                    _args.append(all_args[name])
-            key = [_args[i] for i in self.key_idx]
-            for arg in _args:
-                if hasattr(arg, "dtype"):
-                    key.append(str(arg.dtype))
-            # to avoid encoding conflicts
-            key_s = [str(k) for k in key]
-            key = tuple(key_s)
-            if key not in self.cache:
-                # prune configs
-                used_cached_result = False
-                pruned_configs = self.prune_configs(kwargs)
-                bench_start = time.time()
-                timings = {
-                    config: self._bench(*args, config=config, **kwargs)
-                    for config in pruned_configs
-                }
-                bench_end = time.time()
-                self.bench_time = bench_end - bench_start
-                self.cache[key] = builtins.min(timings, key=timings.get)
-                self._timings[key] = timings[self.cache[key]]
-                if (self.use_cuda_graph and self._timings[key] == float("inf")) or (
-                    not self.use_cuda_graph and self._timings[key][0] == float("inf")
-                ):
-                    raise RuntimeError(
-                        f"All autotune examples failed (timing is {self._timings[key]})."
-                    )
-                self.configs_timings = timings
-                self.pre_hook(args, reset_only=True)
-            config = self.cache[key]
-        else:
-            config = self.configs[0]
-        self.best_config = config
-        if not used_cached_result:
-            global_dejavu_storage.add_autotuner_cache(
-                self.cache,
-                self.fn,
-                self.configs_hash,
-                self.key_hash,
-                self._param_hash,
-                self.configs_len,
-                self._timings,
-                self.rep_t,
-                self.warmup_t,
-                self.bench_time,
-            )
+        given_kwargs = list(kwargs.keys())
+        required_config_args = self.config_kw_names + __additional_config_arg_check__
+        if any(x in given_kwargs for x in required_config_args):
             if os.getenv("TRITON_PRINT_AUTOTUNING", None) == "1":
-                print(
-                    f"Triton autotuning for function {self.base_fn.__name__} finished after "
-                    f"{self.bench_time:.2f}s; best config selected: {self.best_config} with benchmark time {self._timings[key]}; "
-                    f" evaluated {len(pruned_configs)} configurations;"
-                )
-        full_nargs = {**self.nargs, **kwargs, **self.best_config.kwargs}
-        if config.pre_hook is not None:
-            config.pre_hook(full_nargs)
-        if triton_major_version >= 3:
-            if not hasattr(config, "all_kwargs"):
-                config.all_kwargs = lambda: _all_kwargs(config)
+                print(f"Triton autotuning skipped, using given config: {kwargs}.")
             ret = self.fn.run(
                 *args,
                 **kwargs,
-                **config.all_kwargs(),
             )
         else:
-            ret = self.fn.run(
-                *args,
-                num_warps=config.num_warps,
-                num_stages=config.num_stages,
-                num_ctas=config.num_ctas,
-                enable_warp_specialization=config.enable_warp_specialization,
-                **kwargs,
-                **config.kwargs,
-            )
-        self.nargs = None
+            self.nargs = dict(zip(self.arg_names, args))
+            used_cached_result = True
+            self.bench_time = 0.0
+            if len(self.configs) > 1:
+                all_args = {**self.nargs, **kwargs}
+                _args = []
+                for name in self.arg_names:
+                    if name in all_args:
+                        _args.append(all_args[name])
+                key = [_args[i] for i in self.key_idx]
+                for arg in _args:
+                    if hasattr(arg, "dtype"):
+                        key.append(str(arg.dtype))
+                # to avoid encoding conflicts
+                key_s = [str(k) for k in key]
+                key = tuple(key_s)
+                if key not in self.cache:
+                    # prune configs
+                    used_cached_result = False
+                    pruned_configs = self.prune_configs(kwargs)
+                    bench_start = time.time()
+                    timings = {
+                        config: self._bench(*args, config=config, **kwargs)
+                        for config in pruned_configs
+                    }
+                    bench_end = time.time()
+                    self.bench_time = bench_end - bench_start
+                    self.cache[key] = builtins.min(timings, key=timings.get)
+                    self._timings[key] = timings[self.cache[key]]
+                    if (self.use_cuda_graph and self._timings[key] == float("inf")) or (
+                        not self.use_cuda_graph and self._timings[key][0] == float("inf")
+                    ):
+                        raise RuntimeError(
+                            f"All autotune examples failed (timing is {self._timings[key]})."
+                        )
+                    self.configs_timings = timings
+                    self.pre_hook(args, reset_only=True)
+                config = self.cache[key]
+            else:
+                config = self.configs[0]
+            self.best_config = config
+            if not used_cached_result:
+                global_dejavu_storage.add_autotuner_cache(
+                    self.cache,
+                    self.fn,
+                    self.configs_hash,
+                    self.key_hash,
+                    self._param_hash,
+                    self.configs_len,
+                    self._timings,
+                    self.rep_t,
+                    self.warmup_t,
+                    self.bench_time,
+                )
+                if os.getenv("TRITON_PRINT_AUTOTUNING", None) == "1":
+                    print(
+                        f"Triton autotuning for function {self.base_fn.__name__} finished after "
+                        f"{self.bench_time:.2f}s; best config selected: {self.best_config} with benchmark time {self._timings[key]}; "
+                        f" evaluated {len(pruned_configs)} configurations;"
+                    )
+            full_nargs = {**self.nargs, **kwargs, **self.best_config.kwargs}
+            if config.pre_hook is not None:
+                config.pre_hook(full_nargs)
+            if triton_major_version >= 3:
+                if not hasattr(config, "all_kwargs"):
+                    config.all_kwargs = lambda: _all_kwargs(config)
+                ret = self.fn.run(
+                    *args,
+                    **kwargs,
+                    **config.all_kwargs(),
+                )
+            else:
+                ret = self.fn.run(
+                    *args,
+                    num_warps=config.num_warps,
+                    num_stages=config.num_stages,
+                    num_ctas=config.num_ctas,
+                    enable_warp_specialization=config.enable_warp_specialization,
+                    **kwargs,
+                    **config.kwargs,
+                )
+            self.nargs = None
         return ret
 
     def prune_configs(self, kwargs):
