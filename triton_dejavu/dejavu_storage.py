97198e48360e4ba2a23ea01a446d4344a1598cfd
Burkhard Ringlein <ngl@zurich.ibm.com>
2024-07-16 19:43:04 +0200

    typo

--- triton_dejavu/dejavu_storage.py
+++ triton_dejavu/dejavu_storage.py
@@ -181,7 +181,7 @@ class DejavuStorage:
         for folder_name in self.used_configs:
             dir_name = f"{self.storage_path}/{folder_name}/"
             if not os.path.exists(dir_name):
-                os.makedirs(self.dir_name, 0o0777)
+                os.makedirs(dir_name, 0o0777)
             file_name = f"{dir_name}/used_configs.json"
             str_l = [str(c) for c in self.used_configs[folder_name]]
             with open(file_name, "w") as f:
