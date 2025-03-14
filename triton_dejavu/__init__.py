1bf51808c311356b2618a8c816f03b1cdb1db745
Burkhard Ringlein <ngl@zurich.ibm.com>
2025-03-14 18:12:07 +0100

    version increment
    Signed-off-by: Burkhard Ringlein <ngl@zurich.ibm.com>

--- triton_dejavu/__init__.py
+++ triton_dejavu/__init__.py
@@ -15,7 +15,7 @@
 #  *******************************************************************************/
 #
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 
 from .dejavu_storage import global_dejavu_storage
